c0d9bb9f59c48c8dbe3f59c6167a80f3e7c415c5
aiiredha <aliredhataqi1@gmail.com>
2026-09-08 18:38:13 +0000

    Point all Apply buttons to the Google Form

---
+++ src/_data/roles.js
@@ -0,0 +1,9 @@
+module.exports = [
+  { slug: "secretary", title: "Secretary", hasPortfolio: false },
+  { slug: "head-of-media", title: "Head of Media", hasPortfolio: true },
+  { slug: "media-team", title: "Media Team", hasPortfolio: true },
+  { slug: "head-of-events", title: "Head of Events", hasPortfolio: false },
+  { slug: "events-team", title: "Events Team", hasPortfolio: false },
+  { slug: "head-of-public-relations", title: "Head of Public Relations", hasPortfolio: false },
+  { slug: "public-relations-team", title: "Public Relations Team", hasPortfolio: false },
+];
